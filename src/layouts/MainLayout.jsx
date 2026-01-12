a3d1a3509bfacbd64afb8bb9efe5d060f3e526cf
supunyasantha <supunyasantha.official@gmail.com>
2026-01-12 15:33:10 +0530

    Remove Header.

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,12 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Sidebar from "../components/Sidebar";
-import Header from "../components/Header";
+
 const MainLayout = () => {
   return (
     <div className="min-h-screen bg-gray-50 font-sans text-gray-900">
       <Sidebar />
       <div className="ml-64">
-        <Header />
+       
         <main className="p-6">
           <Outlet />
         </main>
