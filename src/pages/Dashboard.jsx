ed36b8775347eabbbeb9cd96193442397a5340d2
supunyasantha <supunyasantha.official@gmail.com>
2026-01-28 13:50:06 +0530

    change searchbar background color

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -22,7 +22,7 @@ const Dashboard = () => {
             <input
               type="text"
               placeholder="Search..."
-              className="pl-9 pr-4 py-2 rounded-full text-sm focus:outline-none border border-black focus:border-black focus:ring-1 focus:ring-black"
+              className="pl-9 pr-4 py-2 rounded-full text-sm focus:outline-none border border-black focus:border-black focus:ring-1 focus:ring-black bg-white"
             />
           </div>
 
@@ -239,8 +239,8 @@ const Dashboard = () => {
                         order.status === "Received"
                           ? "bg-blue-100 text-blue-600"
                           : order.status === "Pending"
-                          ? "bg-yellow-100 text-yellow-600"
-                          : "bg-purple-100 text-purple-600"
+                            ? "bg-yellow-100 text-yellow-600"
+                            : "bg-purple-100 text-purple-600"
                       }`}
                     >
                       {order.status}
